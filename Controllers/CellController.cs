93212d0a12daff8edeec5ca9f8def36e540c2753
jdruiz854 <hurrahfighter@gmail.com>
2022-06-12 15:46:23 -0500

    updated with

---
+++ Controllers/CellController.cs
@@ -0,0 +1,36 @@
+﻿using CST350_CLC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CST350_CLC.Controllers
+{
+    public class CellController : Controller
+    {
+        static List<CellModel> cells = new List<CellModel>();
+        Random random = new Random();
+        const int GRID_SIZE = 100;
+
+        public IActionResult Index()
+        {
+            if(cells.Count < GRID_SIZE)
+            { 
+                for(int i = 0; i < GRID_SIZE; i++)
+                {
+                    cells.Add(new CellModel { Id = i, CellState = 0 });
+                }
+            }
+            return View("Index", cells);
+        }
+
+        public IActionResult HandleCellClick(string cellNumber)
+        {
+            int c = int.Parse(cellNumber);
+
+            cells.ElementAt(c).CellState = (cells.ElementAt(c).CellState + 1) % 2;
+            
+            return View("Index", cells);
+        }
+    }
+}
---
+++ Models/CellModel.cs
@@ -0,0 +1,8 @@
+﻿namespace CST350_CLC.Models
+{
+    public class CellModel
+    {
+        public int Id { get; set; }
+        public int CellState { get; set; }
+    }
+}
